283e4a9839138335681ab34bbce922e46f0f8116
jasonthepenguin <jasonbotterill@hotmail.com>
2021-10-21 16:58:54 +0800

    Arrow object + texture

--- CAMPUS TOUR/src/TextureHandler.cpp
+++ CAMPUS TOUR/src/TextureHandler.cpp
@@ -27,7 +27,7 @@ void CreateTextures()
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
 	// set texture count
-	tp.SetTextureCount(287);
+	tp.SetTextureCount(288);
 
 	// load and create textures
 	image = tp.LoadTexture("data/abovechanctext.raw", 128, 1024);
@@ -866,6 +866,9 @@ void CreateTextures()
 
 	image = tp.LoadTexture("data/Object Textures/Characters/LavaGoblin.raw", 1024, 1024);
 	tp.CreateTexture(286, image, 1024, 1024);
+
+	image = tp.LoadTexture("data/Object Textures/Characters/Arrow.raw", 1024, 1024);
+	tp.CreateTexture(287, image, 1024, 1024);
 
 	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
 		//For lights
